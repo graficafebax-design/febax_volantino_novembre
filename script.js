efb2557549cbdb643e83217a2ab309d7b9203ce0
graficafebax-design <grafica.febax@gmail.com>
2025-11-06 11:27:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-function initFlipbook() {
-  var windowWidth = $(window).width();
-  var windowHeight = $(window).height();
-  
-  var isMobile = windowWidth < 768;
-  var displayMode = isMobile ? 'single' : 'double';
+$(document).ready(function() {
 
-  var width = isMobile ? windowWidth * 0.9 : 1000;   // 1000px desktop
-  var height = isMobile ? windowHeight * 0.9 : 700;  // 700px desktop
+  // Funzione per ridimensionare flipbook
+  function resizeFlipbook() {
+    var width, height, displayMode;
 
+    if (window.innerWidth <= 768) {
+      // Mobile
+      height = window.innerHeight;      // altezza piena dello schermo
+      width = height / 1.414;           // rapporto A4
+      displayMode = 'single';
+    } else {
+      // Desktop
+      width = 842;
+      height = 595;
+      displayMode = 'double';
+    }
+
+    $('#flipbook').turn('size', width, height);
+    $('#flipbook').turn('display', displayMode);
+  }
+
+  // Inizializza Turn.js
   $('#flipbook').turn({
-    width: width,
-    height: height,
+    width: 842,          // larghezza desktop iniziale
+    height: 595,         // altezza desktop iniziale
     autoCenter: true,
-    display: displayMode,
+    display: 'double',   // doppia pagina desktop
     acceleration: true,
     gradients: true,
     elevation: 50,
     duration: 1000,
     pages: $('#flipbook .page').length
   });
-}
 
-// Inizializza al caricamento
-$(document).ready(initFlipbook);
+  // Chiamata iniziale e on resize
+  resizeFlipbook();
+  $(window).resize(resizeFlipbook);
 
-// Ricalcola se si ridimensiona finestra
-$(window).resize(function() {
-  $('#flipbook').turn('destroy');
-  initFlipbook();
 });
-
-// Controlli pulsanti
-$('#prev').click(function(){ $('#flipbook').turn('previous'); });
-$('#next').click(function(){ $('#flipbook').turn('next'); });
-$('#download').click(function(){ alert('Funzione download da implementare'); });
-$('#print').click(function(){ window.print(); });
-$('#home').click(function(){ window.location.href = 'https://www.febax.it'; });
